2c0c93ff197e8503bde08a316877e8e6c4754a10
codersunny812 <sengersunny448@gmail.com>
2024-06-14 11:47:19 +0530

    variables in swift

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+var greeting:String = "Hello, playground"
+print(greeting )
+
+var age : Int = 23
+
+
+var weight : Float = 68.8
+
+var height : Double = 169.234
+
+var isFree : Bool = true
+
+
+var arr : [Int] = [1,2,4,6]
